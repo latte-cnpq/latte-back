8da5a7d7b89c9aec00e0a18f7c154da59df2df11
danimachads <danimachads@gmail.com>
2023-05-01 18:01:10 -0300

    production

--- src/main/java/br/edu/femass/latteback/controllers/ProductionController.java
+++ src/main/java/br/edu/femass/latteback/controllers/ProductionController.java
@@ -1,10 +1,16 @@
 package br.edu.femass.latteback.controllers;
 import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import br.edu.femass.latteback.models.Institute;
 import br.edu.femass.latteback.models.Production;
@@ -13,6 +19,9 @@ import br.edu.femass.latteback.repositories.ProductionRepository;
 import br.edu.femass.latteback.services.ProductionService;
 import br.edu.femass.latteback.services.ResearcherService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -57,4 +66,37 @@ public class ProductionController {
        }
    }
 
+   @GetMapping("/advancedsearch")
+    @Operation(summary = "Find all productions using query search and pagination")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful operation",
+                    content = {@Content(mediaType = "application/json", schema = @Schema(implementation = Page.class))}
+            ),
+    })
+    @Parameters({
+        @Parameter(name = "name", description = "Name of the researcher", in = ParameterIn.QUERY),
+        @Parameter(name = "production_type", description = "Type of production", in = ParameterIn.QUERY),
+        @Parameter(name = "production_details", description = "Details of production", in = ParameterIn.QUERY),
+        @Parameter(name = "page", description = "Page number", in = ParameterIn.QUERY, example = "0"),
+        @Parameter(name = "perPage", description = "Number of items per page", in = ParameterIn.QUERY, example = "10"),
+        @Parameter(name = "ordination", description = "Property used for sorting the results", in = ParameterIn.QUERY, example = "id"),
+        @Parameter(name = "direction", description = "Sort direction (ASC or DESC)", in = ParameterIn.QUERY, example = "ASC")
+    })
+    public ResponseEntity<Object> findByAdvancedSearch(
+        @RequestParam(name = "name", required = false) final String name,
+        @RequestParam(name = "production_type", required = false) final String productionType,
+        @RequestParam(name = "production_details", required = false) final String productionDetails,
+        @RequestParam(defaultValue = "0") final int page,
+            @RequestParam(name = "perPage", defaultValue = "10") final int perPage,
+            @RequestParam(defaultValue = "id") final String ordination,
+            @RequestParam(defaultValue = "ASC") final Sort.Direction direction) {
+            try {
+                final Pageable pageable = PageRequest.of(page, perPage, Sort.by(direction, ordination));
+                Page<Production> productions = null;
+                productions = productionService.AdvancedSearch(name, productionType, productionDetails, pageable);
+                return ResponseEntity.status(HttpStatus.OK).body(productions);
+            } catch (RuntimeException e) {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+            }
+        }
 }
